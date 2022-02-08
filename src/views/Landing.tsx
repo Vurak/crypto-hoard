eb8e11851e4eaaee9f101573b5cb90336f1802d1
Vurak <vurak.of.sulcan@gmail.com>
2022-02-08 22:13:27 +0000

    added basic login UI

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "../components";
+import { Button, Input } from "../components";
 
 const Landing = () => {
   const [expandButton, setExpandButton] = useState(false);
@@ -13,14 +13,16 @@ const Landing = () => {
       <Button
         text="< BACK"
         onClick={() => setExpandButton(false)}
-        classNames="absolute top-0 left-0 m-4"
+        className="absolute top-0 left-0 m-4"
       />
       {expandButton ? (
-        <div className="mx-auto my-5 flex flex-col rounded-lg border-2 border-cyan-400 p-1">
+        <div className="mx-auto my-5 flex flex-col gap-2 rounded-lg border-2 border-cyan-400 p-4">
           <p className="m-auto p-2 text-center align-middle text-lg text-pink-500">
             Welcome to the Internet Merchant
           </p>
-          <input placeholder="test" />
+          <Input placeholder="username" onChange={() => {}} />
+          <Input placeholder="password" onChange={() => {}} />
+          <Button text="Submit" onClick={() => {}} />
         </div>
       ) : (
         <Button onClick={handleUnlockClick} text="UNLOCK" />
