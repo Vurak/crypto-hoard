d4f0b33bf4361f6624dabdd66e4916777293ac3f
Vurak <vurak.of.sulcan@gmail.com>
2022-02-10 00:26:55 +0000

    added Wallet wrapper to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Home } from "./views/Home";
 import Landing from "./views/Landing";
-
+import { Wallet } from "./Wallet";
 function App() {
   return (
-    <div className="flex content-center w-screen h-screen font-mono font-bold bg-zinc-900">
-      <Landing />
-    </div>
+    <Wallet>
+      <div className="flex content-center w-screen h-screen font-mono font-bold bg-zinc-900">
+        <Landing />
+      </div>
+    </Wallet>
   );
 }
 
