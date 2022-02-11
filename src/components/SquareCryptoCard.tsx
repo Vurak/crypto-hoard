366244b9eaffc58534d03679d46c153aacf13422
Vurak <vurak.of.sulcan@gmail.com>
2022-02-11 22:18:58 +0000

    added link to crypto cards and styling + svgs

--- src/components/SquareCryptoCard.tsx
+++ src/components/SquareCryptoCard.tsx
@@ -1,15 +1,22 @@
+import { Link } from "wouter";
+
 type Props = {
+  path: string;
   logo: string;
   children?: JSX.Element | string | null;
 };
 
 export const SquareCryptoCard = ({ children, ...props }: Props) => {
   return (
-    <div className="my-auto rounded-2xl bg-slate-900 p-5 shadow-2xl shadow-slate-900">
-      <div className="flex h-max flex-col text-center">
-        <img src={props.logo} className="mx-auto w-2/5" />
-        <h1 className="h-max truncate py-3 text-2xl text-white">{children}</h1>
+    <Link href={props.path}>
+      <div className="flex h-full rounded-2xl bg-slate-900 p-5 align-middle shadow-2xl shadow-slate-900">
+        <div className="my-auto flex h-max w-full flex-col text-center">
+          <img src={props.logo} className="mx-auto w-2/5" />
+          <h1 className="h-max truncate pt-3 text-2xl text-white">
+            {children}
+          </h1>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,18 +1,20 @@
 import { Link } from "wouter";
 import { SquareCryptoCard } from "../components";
 
+import sol_svg from "../assets/blockchain-logos/sol.svg";
+import eth_svg from "../assets/blockchain-logos/eth.svg";
+import bnb_svg from "../assets/blockchain-logos/bnb.svg";
+
 const Landing = () => {
   return (
     <div className="m-auto grid h-full w-2/3 gap-10 py-10 md:w-2/3 md:grid-flow-col md:grid-cols-3">
-      <Link href="/solana">
-        <SquareCryptoCard logo="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/sol.svg">
-          SOLANA
-        </SquareCryptoCard>
-      </Link>
-      <SquareCryptoCard logo="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/sol.svg">
+      <SquareCryptoCard path="/ethereum" logo={eth_svg}>
         SOLANAsss
       </SquareCryptoCard>
-      <SquareCryptoCard logo="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/sol.svg">
+      <SquareCryptoCard path="/solana" logo={sol_svg}>
+        SOLANA
+      </SquareCryptoCard>
+      <SquareCryptoCard path="/bsc" logo={bnb_svg}>
         test
       </SquareCryptoCard>
     </div>
