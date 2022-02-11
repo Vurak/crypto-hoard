59c31dbec4170b5fdab31c953808e8d24bfe8caf
Vurak <vurak.of.sulcan@gmail.com>
2022-02-11 22:19:09 +0000

    router changes, 404 and switch

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Link, Route } from "wouter";
+import { Route, Switch } from "wouter";
 import { Landing } from "./views";
 const Solana = lazy(() => import("./views/Solana"));
 const SolanaWallet = lazy(() => import("./components/solana/Wallet"));
@@ -7,14 +7,17 @@ const SolanaWallet = lazy(() => import("./components/solana/Wallet"));
 const Router = () => {
   return (
     <Suspense fallback="Loading...">
-      <Route path="/">
-        <Landing />
-      </Route>
-      <Route path="/solana">
-        <SolanaWallet>
-          <Solana />
-        </SolanaWallet>
-      </Route>
+      <Switch>
+        <Route path="/">
+          <Landing />
+        </Route>
+        <Route path="/solana">
+          <SolanaWallet>
+            <Solana />
+          </SolanaWallet>
+        </Route>
+        <Route>404</Route>
+      </Switch>
     </Suspense>
   );
 };
