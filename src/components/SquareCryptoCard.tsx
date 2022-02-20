41c8f824baffef79718a1b4d894179f06201354b
Vurak <vurak.of.sulcan@gmail.com>
2022-02-20 21:02:24 +0000

    allow disabling of home crypto cards

--- src/components/SquareCryptoCard.tsx
+++ src/components/SquareCryptoCard.tsx
@@ -1,15 +1,23 @@
+import React from "react";
 import { Link } from "wouter";
 
 type Props = {
   path: string;
   logo: string;
-  children?: JSX.Element | string | null;
+  disabled?: boolean;
+  children?: React.ReactNode;
 };
 
 export const SquareCryptoCard = ({ children, ...props }: Props) => {
   return (
     <Link href={props.path}>
-      <div className="from-primary-light flex h-full cursor-pointer rounded-2xl bg-gradient-to-tr p-5 align-middle duration-150 hover:bg-primary-accent ">
+      <div
+        className={` ${
+          props.disabled
+            ? "from-gray-800"
+            : "from-primary-light hover:bg-primary-accent cursor-pointer"
+        } flex h-full rounded-2xl bg-gradient-to-tr p-5 align-middle duration-150`}
+      >
         <div className="my-auto flex h-max w-full flex-col text-center">
           <img src={props.logo} className="mx-auto w-2/5" />
           <h1 className="h-max truncate pt-3 text-2xl text-white">
--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -8,14 +8,16 @@ import bnb_svg from "../assets/blockchain-logos/bnb.svg";
 const Landing = () => {
   return (
     <div className="m-auto grid h-full w-2/3 gap-10 py-10 md:w-2/3 md:grid-flow-col md:grid-cols-3">
-      <SquareCryptoCard path="/ethereum" logo={eth_svg}>
+      <SquareCryptoCard disabled path="/" logo={eth_svg}>
         ETHEREUM
+        <p className="text-sm text-gray-300">(in development)</p>
       </SquareCryptoCard>
       <SquareCryptoCard path="/solana" logo={sol_svg}>
         SOLANA
       </SquareCryptoCard>
-      <SquareCryptoCard path="/bsc" logo={bnb_svg}>
+      <SquareCryptoCard disabled path="/" logo={bnb_svg}>
         BINANCE
+        <p className="text-sm text-gray-300">(in development)</p>
       </SquareCryptoCard>
     </div>
   );
