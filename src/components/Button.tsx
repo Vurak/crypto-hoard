de8b42309d0dcfd2b95cb278548d76c3b152b129
Vurak <vurak.of.sulcan@gmail.com>
2022-02-26 18:26:06 +0000

    input and button component chanegs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 
 interface ButtonProps {
-  text?: string;
+  children?: React.ReactNode;
   className?: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
       onClick={props.onClick}
@@ -14,7 +14,7 @@ export const Button = (props: ButtonProps) => {
         props.className || ""
       }`}
     >
-      {props.text || ""}
+      {children || null}
     </button>
   );
 };
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 
 interface Props {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string | number | '';
   placeholder?: string;
   className?: string;
-  onChange: () => void;
 }
 
 export const Input = (props: Props) => {
@@ -11,7 +12,8 @@ export const Input = (props: Props) => {
     <input
       placeholder={props.placeholder}
       onChange={props.onChange}
-      className={`rounded-lg border-2 border-pink-500 bg-zinc-800 p-2 font-bold text-cyan-400 ${props.className}`}
+      value={props.value}
+      className={`rounded-lg border-2 border-primary-accent bg-zinc-800 p-2 font-bold text-white ${props.className}`}
     />
   );
 };
