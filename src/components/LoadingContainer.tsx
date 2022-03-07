08f144201a24039ee70f1cadce66280f421ae463
Vurak <vurak.of.sulcan@gmail.com>
2022-03-07 19:41:13 +0000

    added loader to LoadingContainer

--- src/components/LoadingContainer.tsx
+++ src/components/LoadingContainer.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React from "react"
+import { SpinnerDiamond } from "spinners-react"
 
-type Props = {
-  loading: boolean;
-  fallback?: React.ReactElement;
-  children: JSX.Element | null;
-};
+interface Props {
+  loading: boolean
+  fallback?: React.ReactElement
+  children: JSX.Element | null
+}
 
 export const LoadingContainer = ({ loading, fallback, children }: Props) => {
   if (loading) {
     return (
-      fallback ?? <div className="m-auto h-32 w-32 text-white">Loading</div>
-    );
+      fallback ?? (
+        <div className="flex w-full h-full">
+          <SpinnerDiamond
+            color="white"
+            secondaryColor="#181E51"
+            size="100%"
+            className="m-auto h-1/2 w-1/2 max-h-80 max-w-sm"
+          />
+        </div>
+      )
+    )
   }
-  return children;
-};
+  return children
+}
