63da6e38accbcc4ae6a210bab1cdc3817767173c
Vurak <vurak.of.sulcan@gmail.com>
2022-03-11 23:55:02 +0000

    good about panel working

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
 
   const handleDocumentClick = () => {
     if (menuActive) setMenuActive(false)
+    if (aboutAcitve) setAboutActive(false)
   }
 
   const handleMenuClicks = (menu: string) => {
@@ -30,74 +31,87 @@ function App() {
   }
 
   return (
-    <div
-      onClick={handleDocumentClick}
-      className="min-h-screen w-full bg-primary-dark font-primary font-bold text-white"
-    >
-      <div className="fixed z-30 hidden h-0 w-full items-center md:block md:h-16">
-        <div
-          onClick={() => setLocation("/")}
-          className="absolute top-0 left-0 m-2 flex h-12 cursor-pointer rounded-xl bg-gradient-to-tr from-primary-accent text-white"
-        >
-          <div className="m-auto mx-3 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
+    <>
+      <div
+        onClick={handleDocumentClick}
+        className="min-h-screen w-full bg-primary-dark font-primary font-bold text-white"
+      >
+        <div className="fixed z-30 hidden h-0 w-full items-center md:block md:h-16">
+          <div
+            onClick={() => setLocation("/")}
+            className="absolute top-0 left-0 m-2 flex h-12 cursor-pointer rounded-xl bg-gradient-to-tr from-primary-accent text-white"
+          >
+            <div className="m-auto mx-3 flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+            </div>
+          </div>
+          <div
+            onClick={handleMenuClick}
+            className="absolute top-0 right-0 z-50 m-2 flex h-12 cursor-pointer rounded-xl bg-gradient-to-tr from-primary-accent"
+          >
+            <div className="m-auto mx-3 flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </div>
+          </div>
+          <Transition
+            show={menuActive}
+            enter="transition-opacity duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <DropdownMenu onOptionClick={handleMenuClicks} />
+          </Transition>
+        </div>
+        <div className="relative flex min-h-screen w-full">
+          <Wouter />
+        </div>
+      </div>
+      <Transition
+        show={aboutAcitve}
+        enter="transition-opacity duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0">
+          <div className="fixed inset-0 bg-black opacity-50" />
+          <div className="fixed right-0 h-full w-full p-4 shadow-lg">
+            <AboutPanel />
           </div>
         </div>
-        <div
-          onClick={handleMenuClick}
-          className="absolute top-0 right-0 z-50 m-2 flex h-12 cursor-pointer rounded-xl bg-gradient-to-tr from-primary-accent"
-        >
-          <div className="m-auto mx-3 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </div>
-        </div>
-        <Transition
-          show={menuActive}
-          enter="transition-opacity duration-100"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-150"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <DropdownMenu onOptionClick={handleMenuClicks} />
-        </Transition>
-      </div>
-      <div className="relative flex min-h-screen w-full">
-        <Wouter />
-      </div>
-      {aboutAcitve && (
-        <div className="fixed top-0 z-50 flex min-h-screen w-full">
-          <AboutPanel />
-        </div>
-      )}
-    </div>
+      </Transition>
+    </>
   )
 }
 
--- src/components/AboutPanel.tsx
+++ src/components/AboutPanel.tsx
@@ -1,7 +1,31 @@
-export const AboutPanel = () => {
+export const AboutPanel = ({}) => {
   return (
-    <div className="z-50 m-auto h-5/6 w-5/6 rounded-md bg-slate-600">
-      testestest
+    <div className="relative top-1/2 m-4 mx-auto max-h-screen -translate-y-1/2 overflow-y-scroll rounded-md bg-primary-light p-5 text-white sm:w-4/5 md:w-2/3 lg:w-1/3">
+      <div className="mb-4 flex h-9 w-full text-xl font-bold">
+        About{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="ml-auto h-9 w-9 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </div>
+      <div className="flex w-full">
+        Minim cillum exercitation veniam duis pariatur eu ad ut. Commodo anim
+        aliqua minim ad commodo sunt incididunt culpa Lorem qui Lorem. Et qui
+        pariatur aliqua in ea cillum aliquip nisi sunt ex aliquip deserunt. Sunt
+        enim incididunt tempor culpa Lorem dolore non do cupidatat dolor magna
+        aliquip id quis. Est aliqua cupidatat cupidatat consequat dolor do
+        excepteur officia aute sunt duis velit sit nostrud.
+      </div>
     </div>
   )
 }
